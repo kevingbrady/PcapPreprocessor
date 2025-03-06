5efdf797c0070bf08590b8e1ca623355beaa1c4b
Kevin Brady <kgb@nist.gov>
2025-03-06 18:44:58 +0000

    Update FlowMeterMetrics.py

--- src/FlowMeterMetrics.py
+++ src/FlowMeterMetrics.py
@@ -8,23 +8,8 @@ class FlowMeterMetrics:
 
     def __init__(self, *args, **kwargs):
         self.flows = OrderedDict()
-        self.node_ids = {}
         self.packet_count_total = 0
         self.output_mode = ''
-
-    def get_packet_flow_key(self, packet, direction):
-
-        packet_flow_info = Flow.get_flow_address_info(packet, direction)
-
-        src_ip, dst_ip, src_port, dst_port = packet_flow_info
-
-        if src_ip not in self.node_ids.keys():
-            self.node_ids.update({src_ip: len(self.node_ids)})
-
-        if dst_ip not in self.node_ids.keys():
-            self.node_ids.update({dst_ip: len(self.node_ids)})
-
-        return self.node_ids[src_ip], self.node_ids[dst_ip], int(src_port), int(dst_port)
 
     def process_packet(self, packet) -> (Flow, PacketDirection):
 
@@ -32,22 +17,25 @@ class FlowMeterMetrics:
 
         # Check flow in reverse direction
         direction = PacketDirection.REVERSE
-        # packet_flow_key = Flow.get_packet_flow_key(packet, direction)
-
-        packet_flow_key = self.get_packet_flow_key(packet, direction)
-
+        packet_flow_key = Flow.get_packet_flow_key(packet, direction)
         flow = self.flows.get(packet_flow_key)
 
         if flow is None:
             # Check flow in forward direction
             direction = PacketDirection.FORWARD
-            packet_flow_key = self.get_packet_flow_key(packet, direction)
+            packet_flow_key = Flow.get_packet_flow_key(packet, direction)
             flow = self.flows.get(packet_flow_key)
 
             if flow is None:
                 # If flow does not exist create new flow
                 flow = Flow(packet, direction)
                 self.flows[packet_flow_key] = flow
+
+        '''if flow.packet_time.get_latest_timestamp() > 0 and (packet.time - flow.packet_time.get_latest_timestamp()) > EXPIRED_UPDATE:
+            # If the packet exists in the flow but the packet is sent
+            # after too much of a delay than it is a part of a new flow.
+            flow = Flow(packet, direction)
+            self.flows[packet_flow_key] = flow'''
 
         if 'TCP' in packet:
             if "R" in str(packet['TCP'].flags):
@@ -75,11 +63,14 @@ class FlowMeterMetrics:
 
         flow.flow_sort(packet.time)
 
-        if (self.packet_count_total % GARBAGE_COLLECT_PACKETS) == 0:  # or flow.packet_time.get_flow_duration() > 120:
+        if (self.packet_count_total % GARBAGE_COLLECT_PACKETS) == 0:   # or flow.packet_time.get_flow_duration() > 120:
             self.garbage_collect(packet.time)
+
+        return flow, direction
 
     def garbage_collect(self, latest_time) -> None:
 
         self.flows = {key: flow for key, flow in self.flows.items() if
                       (latest_time - flow.packet_time.get_latest_timestamp()) <= EXPIRED_UPDATE
                       and flow.completed is False}
+
